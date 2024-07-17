4c09e7a8cc2ddb9f7b69f8cc2c4b952c655d5e1d
John Lettman <the@johnlettman.com>
2024-07-17 15:36:34 -0400

    types/pseudo: add column-window

---
+++ types/column-window.go
@@ -0,0 +1,33 @@
+package types
+
+import (
+	"fmt"
+	"github.com/johnlettman/oyster/types/pseudo"
+)
+
+// ColumnWindow is the window over which the sensor fires in columns.
+type ColumnWindow pseudo.ListTupleJSON[int]
+
+// String returns a string representation of the ColumnWindow in the format "V1xV2".
+// The value V1 is the first element in the ColumnWindow and V2 is the second element.
+func (c ColumnWindow) String() string {
+	return fmt.Sprintf("%dx%d", c.V1, c.V2)
+}
+
+// GoString returns a Go syntax representation of the ColumnWindow in the format ColumnWindow{V1, V2}.
+// The value V1 is the first element in the ColumnWindow and V2 is the second element.
+func (c ColumnWindow) GoString() string {
+	return fmt.Sprintf("ColumnWindow{%d, %d}", c.V1, c.V2)
+}
+
+// MarshalJSON returns the JSON encoding of the ColumnWindow by marshaling
+// it as a ListTupleJSON[int].
+func (c ColumnWindow) MarshalJSON() ([]byte, error) {
+	return (pseudo.ListTupleJSON[int])(c).MarshalJSON()
+}
+
+// UnmarshalJSON unmarshals the JSON-encoded data into the ColumnWindow by
+// unmarshaling it as a ListTupleJSON[int].
+func (c *ColumnWindow) UnmarshalJSON(data []byte) error {
+	return (*pseudo.ListTupleJSON[int])(c).UnmarshalJSON(data)
+}
