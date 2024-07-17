bff89601dc40ec49e726ee75b0a05a8528d6b51d
John Lettman <the@johnlettman.com>
2024-07-17 17:33:50 -0400

    metadata/calibration-status: clean up docs

--- metadata/calibration-status.go
+++ metadata/calibration-status.go
@@ -6,14 +6,13 @@ import (
 
 // ReflectivityCalibrationStatus contains the calibration data field from the sensor.
 // Currently, this is solely used for reflectivity calibration details.
+// For additional information, refer to [Calibrated Reflectivity].
 //
-// Ouster recommends contacting [support@ouster.io] if you have questions on whether
+// Ouster recommends contacting [Ouster Support] if you have questions on whether
 // your sensor is hardware-enabled for calibrated reflectivity.
 //
-// For additional information, refer to [Ouster docs: Calibrated Reflectivity].
-//
-// [support@ouster.io]: mailto://support@ouster.io
-// [Ouster docs: Calibrated Reflectivity]: https://static.ouster.dev/sensor-docs/image_route1/image_route2/sensor_data/sensor-data.html#calibrated-reflectivity
+// [Ouster Support]: mailto://support@ouster.io
+// [Calibrated Reflectivity]: https://static.ouster.dev/sensor-docs/image_route1/image_route2/sensor_data/sensor-data.html#calibrated-reflectivity
 type ReflectivityCalibrationStatus struct {
 	// Valid is true if the sensor is factory-calibrated for better accuracy;
 	// otherwise, the sensor is using default values and likely has less accuracy.
