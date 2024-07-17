5a7a5ba51d8076d9c11e54ca378400b1d3fd967b
John Lettman <the@johnlettman.com>
2024-07-16 20:33:12 -0400

    types/multipurpose-io-mode: clean-up docs

--- types/multipurpose-io-mode.go
+++ types/multipurpose-io-mode.go
@@ -30,20 +30,19 @@ import (
 //     defines the output pulse rate.
 //   - In all modes, ConfigurationParameters.SyncPulseOutPulseWidth defines the output pulse width.
 //
-// For additional information, refer to [Ouster docs: multipurpose_io_mode] and
-// [Ouster docs: External Trigger Clock Source]
+// For additional information, refer to [multipurpose_io_mode] and [External Trigger Clock Source].
 //
-// [Ouster docs: External Trigger Clock Source]: https://static.ouster.dev/sensor-docs/image_route1/image_route3/time_sync/time-sync.html#external-trigger-clock-source
-// [Ouster docs: multipurpose_io_mode]: https://static.ouster.dev/sensor-docs/image_route1/image_route2/common_sections/API/sensor_configuration_description.html#multipurpose-io-mode
+// [External Trigger Clock Source]: https://static.ouster.dev/sensor-docs/image_route1/image_route3/time_sync/time-sync.html#external-trigger-clock-source
+// [multipurpose_io_mode]: https://static.ouster.dev/sensor-docs/image_route1/image_route2/common_sections/API/sensor_configuration_description.html#multipurpose-io-mode
 type MultipurposeIOMode uint8
 
 const (
 	MultipurposeOff MultipurposeIOMode = iota // Do not output a SYNC_PULSE_OUT signal.
 
 	// MultipurposeInputNMEAUART reconfigures the MULTIPURPOSE_IO port as an input.
-	// See [Ouster docs: Setting Ouster Sensors Time Source] for more information.
+	// See [Setting Ouster Sensors Time Source] for more information.
 	//
-	// [Ouster docs: Setting Ouster Sensors Time Source]: https://static.ouster.dev/sensor-docs/image_route1/image_route3/time_sync/time-sync.html#setting-sensor-time
+	// [Setting Ouster Sensors Time Source]: https://static.ouster.dev/sensor-docs/image_route1/image_route3/time_sync/time-sync.html#setting-sensor-time
 	MultipurposeInputNMEAUART
 
 	MultipurposeOutputFromInternalOscillator // Output a SYNC_PULSE_OUT signal synchronized with the internal clock.
@@ -52,38 +51,40 @@ const (
 	MultipurposeOutputFromEncoderAngle       // Output a SYNC_PULSE_OUT signal with a user defined rate in an integer number of degrees.
 )
 
-// multipurposeIOModeTextKV maps MultipurposeIOMode values to their text representations.
-var multipurposeIOModeTextKV = map[MultipurposeIOMode]string{
-	MultipurposeOff:                          "OFF",
-	MultipurposeInputNMEAUART:                "INPUT_NMEA_UART",
-	MultipurposeOutputFromInternalOscillator: "OUTPUT_FROM_INTERNAL_OSC",
-	MultipurposeOutputFromSyncPulseIn:        "OUTPUT_FROM_SYNC_PULSE_IN",
-	MultipurposeOutputFromPTP1588:            "OUTPUT_FROM_PTP_1588",
-	MultipurposeOutputFromEncoderAngle:       "OUTPUT_FROM_ENCODER_ANGLE",
-}
+var (
+	// multipurposeIOModeTextKV maps MultipurposeIOMode values to their string representations.
+	multipurposeIOModeStringKV = map[MultipurposeIOMode]string{
+		MultipurposeOff:                          "off",
+		MultipurposeInputNMEAUART:                "input from NMEA UART",
+		MultipurposeOutputFromInternalOscillator: "output from Internal Oscillator",
+		MultipurposeOutputFromSyncPulseIn:        "output from Sync Pulse in",
+		MultipurposeOutputFromPTP1588:            "output from PTP 1588",
+		MultipurposeOutputFromEncoderAngle:       "output from encoder angle",
+	}
 
-// multipurposeIOModeTextKV maps MultipurposeIOMode values to their string representations.
-var multipurposeIOModeStringKV = map[MultipurposeIOMode]string{
-	MultipurposeOff:                          "off",
-	MultipurposeInputNMEAUART:                "input from NMEA UART",
-	MultipurposeOutputFromInternalOscillator: "output from Internal Oscillator",
-	MultipurposeOutputFromSyncPulseIn:        "output from Sync Pulse in",
-	MultipurposeOutputFromPTP1588:            "output from PTP 1588",
-	MultipurposeOutputFromEncoderAngle:       "output from encoder angle",
-}
+	// multipurposeIOModeTextKV maps MultipurposeIOMode values to their Go syntax representations.
+	multipurposeIOModeGoStringKV = map[MultipurposeIOMode]string{
+		MultipurposeOff:                          "MultipurposeOff",
+		MultipurposeInputNMEAUART:                "MultipurposeInputNMEAUART",
+		MultipurposeOutputFromInternalOscillator: "MultipurposeOutputFromInternalOscillator",
+		MultipurposeOutputFromSyncPulseIn:        "MultipurposeOutputFromSyncPulseIn",
+		MultipurposeOutputFromPTP1588:            "MultipurposeOutputFromPTP1588",
+		MultipurposeOutputFromEncoderAngle:       "MultipurposeOutputFromEncoderAngle",
+	}
 
-// multipurposeIOModeTextKV maps MultipurposeIOMode values to their Go syntax representations.
-var multipurposeIOModeGoStringKV = map[MultipurposeIOMode]string{
-	MultipurposeOff:                          "MultipurposeOff",
-	MultipurposeInputNMEAUART:                "MultipurposeInputNMEAUART",
-	MultipurposeOutputFromInternalOscillator: "MultipurposeOutputFromInternalOscillator",
-	MultipurposeOutputFromSyncPulseIn:        "MultipurposeOutputFromSyncPulseIn",
-	MultipurposeOutputFromPTP1588:            "MultipurposeOutputFromPTP1588",
-	MultipurposeOutputFromEncoderAngle:       "MultipurposeOutputFromEncoderAngle",
-}
+	// multipurposeIOModeTextKV maps MultipurposeIOMode values to their text representations.
+	multipurposeIOModeTextKV = map[MultipurposeIOMode]string{
+		MultipurposeOff:                          "OFF",
+		MultipurposeInputNMEAUART:                "INPUT_NMEA_UART",
+		MultipurposeOutputFromInternalOscillator: "OUTPUT_FROM_INTERNAL_OSC",
+		MultipurposeOutputFromSyncPulseIn:        "OUTPUT_FROM_SYNC_PULSE_IN",
+		MultipurposeOutputFromPTP1588:            "OUTPUT_FROM_PTP_1588",
+		MultipurposeOutputFromEncoderAngle:       "OUTPUT_FROM_ENCODER_ANGLE",
+	}
 
-// multipurposeIOModeTextVK maps string representations to MultipurposeIOMode values.
-var multipurposeIOModeTextVK = util.ReverseMap(multipurposeIOModeTextKV)
+	// multipurposeIOModeTextVK maps string representations to MultipurposeIOMode values.
+	multipurposeIOModeTextVK = util.ReverseMap(multipurposeIOModeTextKV)
+)
 
 // String returns the string representation of a MultipurposeIOMode value.
 func (m MultipurposeIOMode) String() string {
