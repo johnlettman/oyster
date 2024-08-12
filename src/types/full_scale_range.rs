c999201207a4dea7d07255fe47bb1def13aba9cd
John Lettman <the@johnlettman.com>
2024-08-12 18:44:29 -0400

    [no ci] types: add tests to FSR

--- src/types/full_scale_range.rs
+++ src/types/full_scale_range.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(Debug, Eq, PartialEq, serde::Serialize, serde::Deserialize, Copy, Clone)]
 #[serde(rename_all = "UPPERCASE")]
 pub enum FullScaleRange {
@@ -8,5 +10,43 @@ pub enum FullScaleRange {
 impl Default for FullScaleRange {
     fn default() -> Self {
         Self::Normal
+    }
+}
+
+impl Display for FullScaleRange {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match *self {
+            Self::Normal => "normal",
+            Self::Extended => "extended"
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use test_log::test;
+    use log::info;
+
+    #[test]
+    fn test_default() {
+        let want = FullScaleRange::Normal;
+        let got = FullScaleRange::default();
+        assert_eq!(want, got);
+    }
+
+    #[test]
+    fn test_display() {
+        let cases = vec![
+            (FullScaleRange::Normal, "normal"),
+            (FullScaleRange::Extended, "extended")
+        ];
+
+        for (fsr, want) in cases {
+            info!("Displaying {fsr:?}, expecting {want:?}");
+            let got = format!("{fsr}");
+            assert_eq!(want, got);
+        }
     }
 }
